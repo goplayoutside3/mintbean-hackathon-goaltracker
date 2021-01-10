1034b044fbe0dc8e5215447ba87fbc1e6ea9ae96
Delilah <delilah>
2021-01-10 16:17:04 -0600

    use next/link to route individual goal pages

---
+++ src/pages/goal/[id].js
@@ -0,0 +1,9 @@
+const SingleGoal = () => {
+  return (
+    <div>
+      This is an individual goal page.
+    </div>
+  )
+}
+
+export default SingleGoal;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,28 @@
-import Head from 'next/head';
 import { useState, useEffect } from 'react';
-import Goal from '../components/Goal'
-import fire from '../config/fire-config'
+
+import Head from 'next/head';
+import Link from 'next/link';
+
+import Goal from '../components/Goal';
+import fire from '../config/fire-config';
 
 const Home = () => {
-  const [goals, setGoals] = useState([])
+  const [goals, setGoals] = useState([]);
 
   useEffect(() => {
-    fire.firestore().collection('goals').onSnapshot(snap => {
-      const goals = snap.docs.map(goal => ({
-        id: goal.id,
-        ...goal.data()
-      }))
-      setGoals(goals)
-    })
+    fire
+      .firestore()
+      .collection('goals')
+      .onSnapshot((snap) => {
+        const goals = snap.docs.map((goal) => ({
+          id: goal.id,
+          ...goal.data(),
+        }));
+        setGoals(goals);
+      });
+  }, []);
 
-  }, [])
-
-  console.log(goals)
+  console.log(goals);
   return (
     <div>
       <Head>
@@ -26,11 +31,17 @@ const Home = () => {
       <h1>Goal Tracker</h1>
       <Goal />
       {goals && goals.length && (
-        <ul>{goals.map(goal => (
-          <li key={goal.id}>{goal.title}</li>
-        ))}</ul>
+        <ul>
+          {goals.map((goal) => (
+            <li key={goal.id}>
+              <Link href="/goal/[id]" as={`/goal/${goal.id}`}>
+                {goal.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
       )}
     </div>
-  )
-}
+  );
+};
 export default Home;
