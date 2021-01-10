63b985f75066adeb407b0c680ce3dfc580b7f774
Delilah <delilah>
2021-01-10 15:27:39 -0600

    initial file structure

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Goal Tracker</title>
+      </Head>
+      <h1>Goal Tracker</h1>
+    </div>
+  )
+}
+export default Home;
