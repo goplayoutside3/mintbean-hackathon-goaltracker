844893423f9a0591ac1bb0bdb4e94c01d0df8b8e
Delilah <delilah>
2021-01-10 16:10:19 -0600

    fetch goals snapshot from fire in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,23 @@
 import Head from 'next/head';
+import { useState, useEffect } from 'react';
 import Goal from '../components/Goal'
+import fire from '../config/fire-config'
 
 const Home = () => {
+  const [goals, setGoals] = useState([])
+
+  useEffect(() => {
+    fire.firestore().collection('goals').onSnapshot(snap => {
+      const goals = snap.docs.map(goal => ({
+        id: goal.id,
+        ...goal.data()
+      }))
+      setGoals(goals)
+    })
+
+  }, [])
+
+  console.log(goals)
   return (
     <div>
       <Head>
@@ -9,6 +25,11 @@ const Home = () => {
       </Head>
       <h1>Goal Tracker</h1>
       <Goal />
+      {goals && goals.length && (
+        <ul>{goals.map(goal => (
+          <li key={goal.id}>{goal.title}</li>
+        ))}</ul>
+      )}
     </div>
   )
 }
