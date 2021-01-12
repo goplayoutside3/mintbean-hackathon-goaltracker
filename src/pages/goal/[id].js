9651fa3957865dc71a1bd9eaa5eb1ef47135f7fd
Delilah <delilah>
2021-01-12 11:43:36 -0600

    write functions for editing individual goal fields

--- src/pages/goal/[id].js
+++ src/pages/goal/[id].js
@@ -3,9 +3,15 @@ import Link from 'next/link';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import styles from '../../styles/pages/goal-id.module.scss';
+import { useState } from 'react';
 
 const SingleGoal = ({ title = '', content = '', goalId }) => {
   const router = useRouter();
+
+  const [goalTitle, setTitle] = useState(title);
+  const [goalContent, setContent] = useState(content);
+  const [editingTitle, setEditingTitle] = useState(false);
+  const [editingContent, setEditingContent] = useState(false);
 
   const handleDelete = (e) => {
     e.preventDefault();
@@ -24,18 +30,92 @@ const SingleGoal = ({ title = '', content = '', goalId }) => {
       });
   };
 
+  const handleSaveTitle = () => {
+    if (!goalTitle.length) return;
+
+    fire
+      .firestore()
+      .collection('goals')
+      .doc(goalId)
+      .update({
+        title: goalTitle,
+      })
+      .then(() => {
+        setEditingTitle(false);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const handleSaveContent = () => {
+    if (!goalContent.length) return;
+
+    fire
+      .firestore()
+      .collection('goals')
+      .doc(goalId)
+      .update({
+        content: goalContent,
+      })
+      .then(() => {
+        setEditingContent(false);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <main className="main">
       <Head>
         <title>{title}</title>
       </Head>
+
+      <Link href="/">
+        <a className={styles.back}>Back to List</a>
+      </Link>
+
       <div className={styles['title-cont']}>
-        <h1>{title}</h1>
-        <Link href="/">
-          <a className={styles.back}>Back to List</a>
-        </Link>
+        {editingTitle ? (
+          <div className={styles.field}>
+            <input
+              className={styles['text-input']}
+              type="text"
+              value={goalTitle}
+              onChange={({ target }) => setTitle(target.value)}
+            />
+            <button onClick={handleSaveTitle}>Save</button>
+          </div>
+        ) : (
+          <div>
+            <button
+              className={styles.edit}
+              onClick={() => setEditingTitle(true)}
+            >
+              <img className={styles['edit-icon']} src="/edit-icon.svg" />
+            </button>
+            <h1 className={styles.title}>{title}</h1>
+          </div>
+        )}
       </div>
-      <p>{content}</p>
+
+      {editingContent ? (
+        <div className={styles.field}>
+          <textarea
+            className={styles['text-input']}
+            value={goalContent}
+            onChange={({ target }) => setContent(target.value)}
+          />
+          <button onClick={handleSaveContent}>Save</button>
+        </div>
+      ) : (
+        <div>
+          <button
+            className={styles.edit}
+            onClick={() => setEditingContent(true)}
+          >
+            <img className={styles['edit-icon']} src="/edit-icon.svg" />
+          </button>
+          <p>{content}</p>
+        </div>
+      )}
       <button onClick={(e) => handleDelete(e)}>Delete This Goal</button>
     </main>
   );
